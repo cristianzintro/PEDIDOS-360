c0e6a300259082cd3fb74234bab2f85a8ab2a2f0
cristianzintro <cris.caceresg@duocuc.cl>
2026-09-21 00:13:06 -0300

    feat: prepare Angular frontend for AWS Amplify

--- frontend/pedidos360-web/src/environments/environment.production.ts
+++ frontend/pedidos360-web/src/environments/environment.production.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://<API_GATEWAY_URL>',
+  apiUrl: 'https://wquye2fcdf.execute-api.us-east-1.amazonaws.com',
   msal: {
     tenantId: '1e723c98-044a-4dc2-bbdf-ebef1ff41d29',
     clientId: '910a32fc-c921-4b26-a90f-ed127214f9bb',
     authority: 'https://login.microsoftonline.com/1e723c98-044a-4dc2-bbdf-ebef1ff41d29',
-    redirectUri: 'https://<FRONTEND_URL>',
+    redirectUri: window.location.origin,
     apiScope: 'api://295f45be-4dfb-4387-9164-48ed7788f505/access_as_user',
   },
 };
